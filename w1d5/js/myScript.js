7c14df76a94a7940992c8a9c71984f38e22acc48
yadu <yaduhat@gmail.com>
2020-04-26 11:32:20 -0500

    updateversion 2

--- w1d5/js/myScript.js
+++ w1d5/js/myScript.js
@@ -1,8 +1,8 @@
 "use strict"
-console.alert("page is loading!")
+alert("page is loading!");
 
 // functional programming 
-// const a=[1,2,3,4,5,6];
+// const a=[1,2,3,4,5,6];//
 
 // const b=a.map(x => x*x).filter(x=>x%2!=0);
 // console.log(b);
